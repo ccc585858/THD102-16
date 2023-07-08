11e3c39ea4802bf1dc8f53974fe93cc16e2e7c3c
pineapple <ccc585858@gmail.com>
2023-07-08 16:28:49 +0800

    0708 rwd fixed

--- js/header.js
+++ js/header.js
@@ -1,6 +1,12 @@
-let hamburger_line = document.getElementsByClassName("hamburger_line")[0];
+// let hamburger_line = document.getElementsByClassName("hamburger_line")[0];
 
-hamburger_line.addEventListener("click", function () {
-  console.log(this.parentElement);
-  this.parentElement.classList.toggle("active");
+// hamburger_line.addEventListener("click", function () {
+//   console.log(this.parentElement);
+//   this.parentElement.classList.toggle("active");
+// });
+
+$(function () {
+  $(".hamburger").on("click", function () {
+    $(".top ul").slideToggle(1000);
+  });
 });
